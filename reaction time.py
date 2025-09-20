edcbedbb862266889558441153f88224c60643f0
KabhiAbhiii <abhics2024@gmail.com>
2025-09-20 20:11:20 +0530

    Add files via upload

---
+++ reaction time.py
@@ -0,0 +1,25 @@
+import keyboard
+import pyautogui
+import time
+
+print("Move the mouse to desired position in 5 sec")
+time.sleep(5)
+x,y=pyautogui.position()
+reply=input("Do you want to scan for a specific color?(Yes/no)")
+if reply.lower()=="yes":
+   print("Start and select the color of the reaction test's go color(press q to stop the code):")
+   ss=pyautogui.screenshot()
+   color=ss.getpixel((x,y))
+elif reply.lower()=="no":
+      print("No screenshot taken")
+else :
+ print("Invalid answer")
+ exit()
+
+while True:
+ check=pyautogui.pixel(x,y)
+ if check==color:
+  pyautogui.click(x,y)
+ if keyboard.is_pressed('q'):
+   break
+ time.sleep(.01) 
